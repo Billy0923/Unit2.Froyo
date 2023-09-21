7a0e134d418691dd669c38a3f4361c3c2d63651a
Billy0923 <Billy5279911@gmail.com>
2023-09-21 16:41:16 -0400

    update catch error for corner case if user did not separate the flavor by ,

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 let input = prompt("enter a list of comma-separated froyo flavors");
 
+try {
+    if(!input.includes(",")) throw "Please seperate the flavors by coma,"
+  }
+  catch(err) {
+    alert(err);
+    input = prompt("enter a list of comma-separated froyo flavors");
+  }
+
 let iArray=input.split(",");
 //console.log(iArray);
 // tested, successfully split the string into array
 
 
 function countFlavor(arr){
+    //empty object
     const FlavorAndCount={
-        
-    };
 
+    };
+    // for each element in the array
+    //
     arr.forEach(x => {
         FlavorAndCount[x]=(FlavorAndCount[x]||0)+1;
     });
 
     return FlavorAndCount;
+    // return the object as rubic requirement
 }
 
 const output=countFlavor(iArray);
